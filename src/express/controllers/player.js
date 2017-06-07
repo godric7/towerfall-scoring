f32671155b405f9750a20f170bbdbf85e7b224f3
godric <godric.com@gmail.com>
2017-06-08 00:58:19 +0200

    Update player screen with more infos

--- src/express/controllers/player.js
+++ src/express/controllers/player.js
@@ -22,32 +22,49 @@ const playerCtrl = (req /*: Request */, res /*: Response */) => {
     const last_date = game.date;
     Object.keys(game.rankings).forEach((opponent) => {
       acc[opponent] = acc[opponent] || {
-        opponent, victories: 0, defeats: 0, ties: 0, total: 0,
+        opponent, victories: 0, defeats: 0, ties: 0, hours: 0, win_streaks: 0, loss_streaks: 0,
       };
-      if (game.rankings[opponent] > game.rankings[player])
+      if (game.rankings[opponent] > game.rankings[player]) {
         acc[opponent].victories += 1;
-      if (game.rankings[opponent] == game.rankings[player])
+        acc[opponent].win_streaks += 1;
+        acc[opponent].loss_streaks = 0;
+      }
+      else if (game.rankings[opponent] == game.rankings[player]) {
         acc[opponent].ties += 1;
-      if (game.rankings[opponent] < game.rankings[player])
+        acc[opponent].win_streaks = 0;
+        acc[opponent].loss_streaks = 0;
+      }
+      else if (game.rankings[opponent] < game.rankings[player]) {
         acc[opponent].defeats += 1;
-      acc[opponent].total += 1;
-      acc[opponent].last_date = game.date;
+        acc[opponent].win_streaks = 0;
+        acc[opponent].loss_streaks += 1;
+      }
+      const time = (new Date(game.date)).getTime();
+      acc[opponent].hours = Math.round((time - Date.now()) / (1000 * 60 * 60));
     });
     return acc;
   }, {});
   delete(encounters[player]);
 
   const victories = Object.keys(encounters)
-    .map((player) => ({ player, count: encounters[player].victories }))
+    .map((opponent) => ({ opponent, count: encounters[opponent].victories }))
     .sort((a , b) => b.count - a.count);
   const defeats = Object.keys(encounters)
-    .map((player) => ({ player, count: encounters[player].defeats }))
+    .map((opponent) => ({ opponent, count: encounters[opponent].defeats }))
     .sort((a , b) => b.count - a.count);
   const ties = Object.keys(encounters)
-    .map((player) => ({ player, count: encounters[player].ties }))
+    .map((opponent) => ({ opponent, count: encounters[opponent].ties }))
     .sort((a , b) => b.count - a.count);
-
-  res.render('player.hbs', { player, rating, victories, defeats, ties });
+  const win_streaks = Object.keys(encounters)
+    .map((opponent) => ({ opponent, count: encounters[opponent].win_streaks }))
+    .sort((a , b) => b.count - a.count);
+  const loss_streaks = Object.keys(encounters)
+    .map((opponent) => ({ opponent, count: encounters[opponent].loss_streaks }))
+    .sort((a , b) => b.count - a.count);
+  const hours = Object.keys(encounters)
+    .map((opponent) => ({ opponent, count: encounters[opponent].hours }))
+    .sort((a , b) => b.count - a.count);
+  res.render('player.hbs', { player, rating, victories, defeats, ties, win_streaks, loss_streaks, hours });
 };
 
 module.exports = {
