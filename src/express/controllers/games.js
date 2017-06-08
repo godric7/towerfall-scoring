fa3b3c105a240efba5a4cfd86604faa4cb15309f
Laurent Grima <grima.laurent@gmail.com>
2017-06-08 13:06:14 +0200

    Sort games by most recent in Games controller

--- src/express/controllers/games.js
+++ src/express/controllers/games.js
@@ -11,12 +11,14 @@ const gamesCtrl = (req /*: Request */, res /*: Response */) => {
   const store = req.app.get('store');
   const state /*: State */ = store.getState();
 
-  const games = state.games.reverse().map((game) => ({
-    date: game.date,
-    rankings: Object.keys(game.rankings)
-      .map((player) => ({ player, rank: game.rankings[player] }))
-      .sort((a, b) => a.rank - b.rank),
-  }));
+  const games = state.games
+    .sort((g1, g2) => g2.date.localeCompare(g1.date))
+    .map((game) => ({
+      date: game.date,
+      rankings: Object.keys(game.rankings)
+        .map((player) => ({ player, rank: game.rankings[player] }))
+        .sort((a, b) => a.rank - b.rank),
+    }));
   res.render('games.hbs', { games });
 };
 
