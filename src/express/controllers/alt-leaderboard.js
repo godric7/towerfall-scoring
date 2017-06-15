5bc85efa721d8851b70b26899c42626064bdc1e8
Christophe Blefari <christophe.blefari@gmail.com>
2017-06-15 09:06:17 +0200

    Clean reduceCount method

--- src/express/controllers/alt-leaderboard.js
+++ src/express/controllers/alt-leaderboard.js
@@ -14,30 +14,12 @@ const altLeaderBoardCtrl = (req /*: Request */, res /*: Response */) => {
     games
     .map(game => game.rankings)
     .map(game => Object.keys(game))
-  ).reduce((output, player) => {
-      const oldValue = output[player];
-
-      if (oldValue == undefined) {
-        output[player] = 1;
-      } else {
-        output[player] = oldValue + 1;
-      }
-      return output
-    }, {});
+  ).reduce(reduceCount, {});
 
   const firstRankPerPlayer = games
     .map(game => game.rankings)
     .map(game => Object.keys(game).find(key => game[key] == 1))
-    .reduce((output, player) => {
-      const oldValue = output[player];
-
-      if (oldValue == undefined) {
-        output[player] = 1;
-      } else {
-        output[player] = oldValue + 1;
-      }
-      return output
-    }, {});
+    .reduce(reduceCount, {});
 
   const scores = Object.keys(countAllGamesPerPlayer)
     .map(player => ({player, rating: (firstRankPerPlayer[player] / countAllGamesPerPlayer[player]) || 0}))
@@ -48,7 +30,18 @@ const altLeaderBoardCtrl = (req /*: Request */, res /*: Response */) => {
         rating: Math.round(a.rating * 10000) / 100,
       }));
 
-    res.render('index.hbs', { scores });
+  res.render('index.hbs', { scores });
+};
+
+const reduceCount = (mergeObject /*: Object */, key /*: String */) => {
+  const oldValue = mergeObject[key];
+
+  if (oldValue == undefined) {
+    mergeObject[key] = 1;
+  } else {
+    mergeObject[key] = oldValue + 1;
+  }
+  return mergeObject
 };
 
 module.exports = {
